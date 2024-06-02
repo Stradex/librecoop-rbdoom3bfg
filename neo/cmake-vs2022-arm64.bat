3a944086078453c75360d05eff721ad3fe6c947a
Robert Beckebans <robert.beckebans@gmail.com>
2024-06-02 16:11:19 +0200

    Fixed typo in cmake-vs2022-arm64.bat

--- neo/cmake-vs2022-arm64.bat
+++ neo/cmake-vs2022-arm64.bat
@@ -2,5 +2,5 @@ cd ..
 del /s /q build
 mkdir build
 cd build
-cmake -G "Visual Studio 17" -A arm64 -DUSE_INTRINSICS_SSE=OFF VULKAN=OFF -DFFMPEG=OFF -DBINKDEC=ON ../neo 
+cmake -G "Visual Studio 17" -A arm64 -DUSE_INTRINSICS_SSE=OFF -DVULKAN=OFF -DFFMPEG=OFF -DBINKDEC=ON ../neo 
 pause
