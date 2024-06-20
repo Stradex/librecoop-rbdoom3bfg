a9cb4bd314e0698311244f62f99ed2a187e1066d
Robert Beckebans <robert.beckebans@gmail.com>
2024-06-20 20:31:05 +0200

    Imtui hello world

---
+++ neo/libs/imtui/imtui-demo.h
@@ -0,0 +1,17 @@
+/*! \file imtui-demo.h
+ *  \brief Enter description here.
+ */
+
+#pragma once
+
+#include "imtui/imtui.h"
+
+namespace ImTui
+{
+
+void ShowAboutWindow( bool* );
+void ShowDemoWindow( bool* );
+void ShowUserGuide();
+void ShowStyleEditor( ImGuiStyle* style = nullptr );
+
+}
--- neo/tools/compilers/main.cpp
+++ neo/tools/compilers/main.cpp
@@ -689,6 +689,8 @@ int com_editors = 0;
 ==============================================================
 */
 
+#if 0
+
 int main( int argc, char** argv )
 {
 	idLib::common = common;
@@ -727,3 +729,64 @@ int main( int argc, char** argv )
 
 	return 0;
 }
+
+#else
+
+#include "imtui/imtui.h"
+#include "imtui/imtui-impl-ncurses.h"
+#include "imtui/imtui-demo.h"
+
+int main()
+{
+	IMGUI_CHECKVERSION();
+	ImGui::CreateContext();
+
+	auto screen = ImTui_ImplNcurses_Init( true );
+	ImTui_ImplText_Init();
+
+	bool demo = true;
+	int nframes = 0;
+	float fval = 1.23f;
+
+	while( true )
+	{
+		ImTui_ImplNcurses_NewFrame();
+		ImTui_ImplText_NewFrame();
+
+		ImGui::NewFrame();
+
+		ImGui::SetNextWindowPos( ImVec2( 4, 27 ), ImGuiCond_Once );
+		ImGui::SetNextWindowSize( ImVec2( 50.0, 10.0 ), ImGuiCond_Once );
+		ImGui::Begin( "Hello, world!" );
+		ImGui::Text( "NFrames = %d", nframes++ );
+		ImGui::Text( "Mouse Pos : x = %g, y = %g", ImGui::GetIO().MousePos.x, ImGui::GetIO().MousePos.y );
+		ImGui::Text( "Time per frame %.3f ms/frame (%.1f FPS)", 1000.0f / ImGui::GetIO().Framerate, ImGui::GetIO().Framerate );
+		ImGui::Text( "Float:" );
+		ImGui::SameLine();
+		ImGui::SliderFloat( "##float", &fval, 0.0f, 10.0f );
+
+#ifndef __EMSCRIPTEN__
+		ImGui::Text( "%s", "" );
+		if( ImGui::Button( "Exit program", { ImGui::GetContentRegionAvail().x, 2 } ) )
+		{
+			break;
+		}
+#endif
+
+		ImGui::End();
+
+		ImTui::ShowDemoWindow( &demo );
+
+		ImGui::Render();
+
+		ImTui_ImplText_RenderDrawData( ImGui::GetDrawData(), screen );
+		ImTui_ImplNcurses_DrawScreen();
+	}
+
+	ImTui_ImplText_Shutdown();
+	ImTui_ImplNcurses_Shutdown();
+
+	return 0;
+}
+
+#endif
